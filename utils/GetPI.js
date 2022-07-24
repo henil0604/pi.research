4223e87819514f90e65e395ed76d60ce8354f9f9
henil0604 <henilmalaviya06@gmail.com>
2022-07-24 16:22:01 +0530

    feature: cache system

--- utils/GetPI.js
+++ utils/GetPI.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env zx
+const Filic = require("filic");
+const Path = require("path");
+const fs = new Filic(process.cwd());
+const PIResearchDir = fs.open("dir:.pi.research");
+const PIFile = PIResearchDir.open("file:value-of-pi");
+
 
 const FetchPI = async (start = 0, number = 1000) => {
     const res = await fetch(`https://api.pi.delivery/v1/pi?start=${start}&numberOfDigits=${number}`);
@@ -10,29 +16,75 @@ const FetchPI = async (start = 0, number = 1000) => {
     }
 }
 
+let Cache = {};
+
+Cache.set = (PI = "") => {
+    if (PIFile.content?.length >= PI.length) {
+        return false;
+    }
+
+    PIFile.write(PI)
+
+    return true;
+}
+
+Cache.get = () => {
+    return PIFile.content || null
+}
+
+Cache.cachedDigits = () => {
+    return PIFile.content?.length || 0
+}
+
+Cache.reset = () => {
+    PIFile.delete()
+    return true;
+}
+
+const ApplyDot = (PI) => {
+    return "3." + PI.slice(1, PI.length);
+}
+
 const GetPI = async (numberOfDigits, dot = false, MAX = 1000) => {
     let PI = "";
 
-    const count = numberOfDigits / MAX;
-    const integer = Math.floor(count);
+    const cachedPI = Cache.get();
+    const cachedDigits = Cache.cachedDigits();
+    const absoluteNumberOfDigits = numberOfDigits - cachedDigits;
 
-    for (let i = 1; i <= integer; i++) {
-        const pi = await FetchPI(PI.length, MAX);
-
-        PI += pi;
+    if (cachedPI !== null && cachedDigits >= numberOfDigits) {
+        return cachedPI;
     }
 
-    const remaining = numberOfDigits - PI.length;
-    if (remaining !== 0) {
-        const remainingPi = await FetchPI(PI.length, remaining);
-        PI += remainingPi;
-    }
+    if (cachedDigits <= numberOfDigits) {
+        if (cachedPI !== null) {
+            PI = cachedPI
+        }
 
-    if (dot === true) {
-        PI = "3." + PI.slice(1, PI.length)
+        const count = absoluteNumberOfDigits / MAX;
+        const integer = Math.floor(count);
+
+        for (let i = 1; i <= integer; i++) {
+            const pi = await FetchPI(PI.length, MAX);
+            PI += pi;
+        }
+
+        const remaining = numberOfDigits - PI.length;
+        if (remaining !== 0) {
+            const remainingPi = await FetchPI(PI.length, remaining);
+            PI += remainingPi;
+        }
+
+        if (dot === true) {
+            PI = ApplyDot(PI);
+        }
     }
+    Cache.set(PI);
 
     return PI;
 }
+
+GetPI.Cache = Cache;
+GetPI.ApplyDot = ApplyDot;
 
 module.exports = GetPI;
