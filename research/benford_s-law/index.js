82925dfd4e1227c7642a07749154a7699f0732d5
henil0604 <henilmalaviya06@gmail.com>
2022-07-24 14:27:51 +0530

    rename: index.js -> index.mjs

--- research/benford_s-law/index.js
+++
@@ -1,1 +0,0 @@
-console.log("Benford's Law")
---
+++ research/benford_s-law/index.mjs
@@ -0,0 +1,48 @@
+const GetPI = require("../../utils/GetPI");
+
+// This Data Object Represents the number of counted digit and total Number of digits that was counted
+let Data = {
+    numbers: {
+        0: {
+            count: 0,
+            percentage: 0,
+        },
+        1: {
+            count: 0,
+            percentage: 0,
+        },
+        2: {
+            count: 0,
+            percentage: 0,
+        },
+        3: {
+            count: 0,
+            percentage: 0,
+        },
+        4: {
+            count: 0,
+            percentage: 0,
+        },
+        5: {
+            count: 0,
+            percentage: 0,
+        },
+        6: {
+            count: 0,
+            percentage: 0,
+        },
+        7: {
+            count: 0,
+            percentage: 0,
+        },
+        8: {
+            count: 0,
+            percentage: 0,
+        },
+        9: {
+            count: 0,
+            percentage: 0,
+        }
+    },
+    totalDigits: 0
+};
